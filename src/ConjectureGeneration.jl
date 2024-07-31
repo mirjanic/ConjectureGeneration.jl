f73ee907d4e8d3fa4d86a7077907858e23c522d5
Viktor Mirjanic <vvm22@cam.ac.uk>
2024-07-31 14:10:33 +0200

    Initial codebase

---
+++ src/ConjectureGeneration.jl
@@ -0,0 +1,12 @@
+module ConjectureGeneration
+
+export SymbolicObjective
+export HelperFunctions
+export SignumLoss
+
+include("SymbolicObjective.jl")
+include("HelperFunctions.jl")
+include("Domain.jl")
+include("SignumLoss.jl")
+
+end
